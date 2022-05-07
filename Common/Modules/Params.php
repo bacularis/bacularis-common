4b8cfc28f686b6ee42e191b1aced20f3dcbbd487
Marcin Haba <marcin.haba@bacula.pl>
2022-05-07 16:32:34 +0200

    Update version

--- Common/Modules/Params.php
+++ Common/Modules/Params.php
@@ -40,7 +40,7 @@ use Bacularis\Common\Modules\CommonModule;
  */
 class Params extends CommonModule {
 
-	const BACULUM_VERSION = '1.0.8';
+	const BACULUM_VERSION = '1.0.9';
 
 	public static $months = array(
 		'jan' => 'January',
