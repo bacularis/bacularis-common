dce7ce9392faa5d0fcf28abaf757dd6e1facb85a
Marcin Haba <marcin.haba@bacula.pl>
2022-09-24 18:14:42 +0200

    Update version

--- Common/Modules/Params.php
+++ Common/Modules/Params.php
@@ -37,7 +37,7 @@ namespace Bacularis\Common\Modules;
  */
 class Params extends CommonModule
 {
-	public const BACULARIS_VERSION = '1.1.1';
+	public const BACULARIS_VERSION = '1.2.0';
 
 	public static $months = [
 		'jan' => 'January',
