826dddb70d08dbf7c6a4f15eadb14f2050615eaa
Marcin Haba <marcin.haba@bacula.pl>
2024-12-04 07:20:28 +0100

    Prepare interface for new Bacula backup plugin type

---
+++ Common/Modules/IBaculaBackupPlugin.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * Bacularis - Bacula web interface
+ *
+ * Copyright (C) 2021-2024 Marcin Haba
+ *
+ * The main author of Bacularis is Marcin Haba, with contributors, whose
+ * full list can be found in the AUTHORS file.
+ *
+ * You may use this file and others of this release according to the
+ * license defined in the LICENSE file, which includes the Affero General
+ * Public License, v3.0 ("AGPLv3") and some additional permissions and
+ * terms pursuant to its AGPLv3 Section 7.
+ */
+
+namespace Bacularis\Common\Modules;
+
+/**
+ * Interface for backup plugin type.
+ *
+ * @author Marcin Haba <marcin.haba@bacula.pl>
+ * @category Interface
+ */
+interface IBaculaBackupPlugin extends IBacularisPlugin
+{
+	/**
+	 * Plugin module initialize method.
+	 *
+	 * @param array $args plugin arguments
+	 */
+	public function initialize(array $args): void;
+
+	/**
+	 * Backup command.
+	 *
+	 * @param array $args backup command arguments
+	 * @return bool true on success, false otherwise
+	 */
+	public function doBackup(array $args): bool;
+
+	/**
+	 * Restore command.
+	 *
+	 * @param array $args restore command arguments
+	 * @return bool true on success, false otherwise
+	 */
+	public function doRestore(array $args): bool;
+
+	/**
+	 * Get plugin command to put in fileset.
+	 *
+	 * @param string $action plugin command action
+	 * @param array $params plugin command parameters
+	 * @return string plugin command
+	 */
+	public function getPluginCommand(string $action, array $params): array;
+
+	/**
+	 * Get plugin command list returned by main fileset plugin command.
+	 * (@see IBaculaBackupPlugin::getPluginCommand);
+	 *
+	 * @param string $action plugin command action
+	 * @param array $params plugin command parameters
+	 * @return string plugin command
+	 */
+	public function getPluginCommands(array $args): array;
+
+	/**
+	 * Get plugin backup parameter categories.
+	 * It should return all parameter categories that are used in backup.
+	 *
+	 * @return array plugin parameter categories
+	 */
+	public static function getBackupParameterCategories(): array;
+
+	/**
+	 * Get plugin restore parameter categories.
+	 * It should return all parameter categories that are used in restore.
+	 *
+	 * @return array plugin parameter categories
+	 */
+	public static function getRestoreParameterCategories(): array;
+}
