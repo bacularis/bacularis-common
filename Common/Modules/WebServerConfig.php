7c8f5fed4e01b60ccab7e062f6f14f99f7534f93
Marcin Haba <marcin.haba@bacula.pl>
2025-01-14 05:15:23 +0100

    Add change web server port support

--- Common/Modules/WebServerConfig.php
+++ Common/Modules/WebServerConfig.php
@@ -21,45 +21,16 @@ namespace Bacularis\Common\Modules;
  * @author Marcin Haba <marcin.haba@bacula.pl>
  * @category Module
  */
-class WebServerConfig extends CommonModule
+class WebServerConfig extends ShellCommandModule
 {
-	/**
-	 * Sudo command.
-	 *
-	 * @var string
-	 */
-	private const SUDO = 'sudo -S';
-
-	/**
-	 * Set common command parameters.
-	 *
-	 * @param array $command command reference
-	 * @param array $params command parameters
-	 */
-	private static function setCommandParameters(array &$command, array $params)
-	{
-		if (key_exists('use_shell', $params) && $params['use_shell']) {
-			$command = array_map(
-				fn ($item) => str_replace(['"'], ['\\\"'], $item),
-				$command
-			);
-			array_unshift($command, 'sh', '-c', '"');
-			array_push($command, '"');
-		}
-		if (key_exists('use_sudo', $params) && $params['use_sudo']) {
-			array_unshift($command, self::SUDO);
-		}
-		array_unshift($command, 'LANG=C');
-	}
-
 	/**
 	 * Get command to enable HTTPS connection in the Nginx web server configuration file.
 	 *
 	 * @param string $package_type OS package type: rpm or deb
-	 * @param array $params command options
+	 * @param array $cmd_params command options
 	 * @return array command to enable HTTPS in Nginx config
 	 */
-	public static function getEnableHTTPSNginxCommand(string $package_type, array $params = []): array
+	public static function getEnableHTTPSNginxCommand(string $package_type, array $cmd_params = []): array
 	{
 		$cfg_path = self::getNginxConfigFilePath($package_type);
 		$key_file = SSLCertificate::getKeyFilePath();
@@ -70,9 +41,10 @@ class WebServerConfig extends CommonModule
 			...$disable_https_cmd,
 			';',
 			'sed',
+			'-E',
 			'-i',
 			'-e',
-			'"s/listen 9097;$/listen 9097 ssl;/"',
+			'"s/listen (.*[0-9]+);$/listen \\\1 ssl;/"',
 			'-e',
 			'"/index index.php;/a ssl_certificate_key ' . $key_file . ';"',
 			'-e',
@@ -81,8 +53,8 @@ class WebServerConfig extends CommonModule
 			';',
 			'}'
 		];
-		$params['use_shell'] = true; // force using shell
-		self::setCommandParameters($ret, $params);
+		$cmd_params['use_shell'] = true; // force using shell
+		static::setCommandParameters($ret, $cmd_params);
 		return $ret;
 	}
 
@@ -90,32 +62,59 @@ class WebServerConfig extends CommonModule
 	 * Get command to disable HTTPS connection in the Nginx web server configuration file.
 	 *
 	 * @param string $package_type OS package type: rpm or deb
-	 * @param array $params command options
+	 * @param array $cmd_params command options
 	 * @return array command to disable HTTPS in Nginx config
 	 */
-	public static function getDisableHTTPSNginxCommand(string $package_type, array $params = []): array
+	public static function getDisableHTTPSNginxCommand(string $package_type, array $cmd_params = []): array
 	{
 		$cfg_path = self::getNginxConfigFilePath($package_type);
 		$ret = [
 			'sed',
+			'-E',
 			'-i',
 			'-e',
 			'"/ssl_certificate/d"',
 			'-e',
-			'"s/listen 9097 ssl;$/listen 9097;/"',
+			'"s/listen (.*[0-9]+) ssl;$/listen \\\1;/"',
 			$cfg_path
 		];
-		self::setCommandParameters($ret, $params);
+		static::setCommandParameters($ret, $cmd_params);
+		return $ret;
+	}
+
+	/**
+	 * Get command to set listening port in the Nginx web server configuration file.
+	 * NOTE: The sed first string occurence substitution works only with GNU sed version.
+	 *
+	 * @param string $package_type OS package type: rpm or deb
+	 * @param integer $port port to set
+	 * @param array $cmd_params command optionsa
+	 * @return array command to set listening port in Nginx config
+	 */
+	public static function getChangeNginxPortCommand(string $package_type, int $port, array $cmd_params = []): array
+	{
+		$cfg_path = self::getNginxConfigFilePath($package_type);
+		$ret = [
+			'sed',
+			'-E',
+			'-i',
+			'-e',
+			'"0,/listen .*:?[0-9]+[^0-9]*;/{s/listen (.+:)?([0-9]+)([^0-9]*);$/listen \\\1XXPORTXX\\\3;/}"',
+			'-e',
+			'"s/XXPORTXX/' . $port . '/"',
+			$cfg_path
+		];
+		static::setCommandParameters($ret, $cmd_params);
 		return $ret;
 	}
 
 	/**
 	 * Get command to enable HTTPS connection in the Lighttpd web server configuration file.
 	 *
-	 * @param array $params command options
+	 * @param array $cmd_params command options
 	 * @return array command to enable HTTPS in Nginx config
 	 */
-	public static function getEnableHTTPSLighttpdCommand(array $params = []): array
+	public static function getEnableHTTPSLighttpdCommand(array $cmd_params = []): array
 	{
 		$cfg_path = self::getLighttpdConfigFilePath();
 		$pem_file = SSLCertificate::getPEMFilePath();
@@ -135,18 +134,18 @@ ssl.pemfile = "' . $pem_file . '"
 			';',
 			'}'
 		];
-		$params['use_shell'] = true; // force using shell
-		self::setCommandParameters($ret, $params);
+		$cmd_params['use_shell'] = true; // force using shell
+		static::setCommandParameters($ret, $cmd_params);
 		return $ret;
 	}
 
 	/**
 	 * Get command to disable HTTPS connection in the Lighttpd web server configuration file.
 	 *
-	 * @param array $params command options
+	 * @param array $cmd_params command options
 	 * @return array command to disable HTTPS in Nginx config
 	 */
-	public static function getDisableHTTPSLighttpdCommand(array $params = []): array
+	public static function getDisableHTTPSLighttpdCommand(array $cmd_params = []): array
 	{
 		$cfg_path = self::getLighttpdConfigFilePath();
 		$ret = [
@@ -158,7 +157,29 @@ ssl.pemfile = "' . $pem_file . '"
 			'"/mod_openssl/d"',
 			$cfg_path,
 		];
-		self::setCommandParameters($ret, $params);
+		static::setCommandParameters($ret, $cmd_params);
+		return $ret;
+	}
+
+	/**
+	 * Get command to set listening port in the Lighttpd web server configuration file.
+	 *
+	 * @param integer $port port to set
+	 * @param array $cmd_params command optionsa
+	 * @return array command to set listening port in Lighttpd config
+	 */
+	public static function getChangeLighttpdPortCommand(int $port, array $cmd_params = []): array
+	{
+		$cfg_path = self::getLighttpdConfigFilePath();
+		$ret = [
+			'sed',
+			'-E',
+			'-i',
+			'-e',
+			'"s/server.port\\\s*=\\\s*[0-9]+$/server.port = ' . $port . '/"',
+			$cfg_path
+		];
+		static::setCommandParameters($ret, $cmd_params);
 		return $ret;
 	}
 
@@ -166,10 +187,10 @@ ssl.pemfile = "' . $pem_file . '"
 	 * Get command to enable HTTPS connection in the Apache web server configuration file.
 	 *
 	 * @param string $package_type OS package type: rpm or deb
-	 * @param array $params command options
+	 * @param array $cmd_params command options
 	 * @return array command to enable HTTPS in Apache config
 	 */
-	public static function getEnableHTTPSApacheCommand(string $package_type, array $params = []): array
+	public static function getEnableHTTPSApacheCommand(string $package_type, array $cmd_params = []): array
 	{
 		$cfg_path = self::getApacheConfigFilePath($package_type);
 		$key_file = SSLCertificate::getKeyFilePath();
@@ -195,8 +216,8 @@ ssl.pemfile = "' . $pem_file . '"
 			';',
 			'}'
 		];
-		$params['use_shell'] = true; // force using shell
-		self::setCommandParameters($ret, $params);
+		$cmd_params['use_shell'] = true; // force using shell
+		static::setCommandParameters($ret, $cmd_params);
 		return $ret;
 	}
 
@@ -204,10 +225,10 @@ ssl.pemfile = "' . $pem_file . '"
 	 * Get command to disable HTTPS connection in the Apache web server configuration file.
 	 *
 	 * @param string $package_type OS package type: rpm or deb
-	 * @param array $params command options
+	 * @param array $cmd_params command options
 	 * @return array command to disable HTTPS in Apache config
 	 */
-	public static function getDisableHTTPSApacheCommand(string $package_type, array $params = []): array
+	public static function getDisableHTTPSApacheCommand(string $package_type, array $cmd_params = []): array
 	{
 		$cfg_path = self::getApacheConfigFilePath($package_type);
 		$ret = [
@@ -219,7 +240,35 @@ ssl.pemfile = "' . $pem_file . '"
 			'"/SSLCertificate/d"',
 			$cfg_path
 		];
-		self::setCommandParameters($ret, $params);
+		static::setCommandParameters($ret, $cmd_params);
+		return $ret;
+	}
+
+	/**
+	 * Get command to set listening port in the Apache web server configuration file.
+	 * NOTE: The sed first string occurence substitution works only with GNU sed version.
+	 *
+	 * @param string $package_type OS package type: rpm or deb
+	 * @param integer $port port to set
+	 * @param array $cmd_params command optionsa
+	 * @return array command to set listening port in Apache config
+	 */
+	public static function getChangeApachePortCommand(string $package_type, int $port, array $cmd_params = []): array
+	{
+		$cfg_path = self::getApacheConfigFilePath($package_type);
+		$ret = [
+			'sed',
+			'-E',
+			'-i',
+			'-e',
+			'"0,/Listen [A-Za-z0-9.]*:?[0-9]+$/{s/Listen ([A-Za-z0-9.]*:?)?([0-9]+)$/Listen \\\1XXPORTXX/}"',
+			'-e',
+			'"s/XXPORTXX/' . $port . '/"',
+			'-e',
+			'"0,/<VirtualHost [A-Za-z0-9._*]*:[0-9]+>/{s/<VirtualHost ([A-Za-z0-9._*]*):([0-9]+)>/<VirtualHost \\\1:' . $port . '>/}"',
+			$cfg_path
+		];
+		static::setCommandParameters($ret, $cmd_params);
 		return $ret;
 	}
 
@@ -290,10 +339,10 @@ ssl.pemfile = "' . $pem_file . '"
 	 * Reload Apache web server configuration.
 	 *
 	 * @param string $package_type OS package type: rpm or deb
-	 * @param array $params command options
+	 * @param array $cmd_params command options
 	 * @return array Apache web server reload command
 	 */
-	public static function getApacheReloadCommand(string $package_type, array $params = []): array
+	public static function getApacheReloadCommand(string $package_type, array $cmd_params = []): array
 	{
 		$ret = [];
 		if ($package_type == 'rpm') {
@@ -309,41 +358,166 @@ ssl.pemfile = "' . $pem_file . '"
 				'apache2'
 			];
 		}
-		self::setCommandParameters($ret, $params);
+		static::setCommandParameters($ret, $cmd_params);
 		return $ret;
 	}
 
 	/**
 	 * Reload Nginx web server configuration.
 	 *
-	 * @param array $params command options
+	 * @param array $cmd_params command options
 	 * @return array Nginx web server reload command
 	 */
-	public static function getNginxReloadCommand(array $params = []): array
+	public static function getNginxReloadCommand(array $cmd_params = []): array
 	{
 		$ret = [
 			'systemctl',
 			'reload',
 			'nginx'
 		];
-		self::setCommandParameters($ret, $params);
+		static::setCommandParameters($ret, $cmd_params);
 		return $ret;
 	}
 
 	/**
 	 * Reload Lighttpd web server configuration.
 	 *
-	 * @param array $params command options
+	 * @param array $cmd_params command options
 	 * @return array Lighttpd web server reload command
 	 */
-	public static function getLighttpdRestartCommand(array $params = []): array
+	public static function getLighttpdRestartCommand(array $cmd_params = []): array
 	{
 		$ret = [
 			'systemctl',
 			'restart',
 			'bacularis-lighttpd'
 		];
-		self::setCommandParameters($ret, $params);
+		static::setCommandParameters($ret, $cmd_params);
 		return $ret;
+	}
+
+	/**
+	 * Enable HTTPS protocol in web server configuration file.
+	 *
+	 * @param string $repository_type system repository type (rpm, deb)
+	 * @param string $web_server web server name (apache, nginx, lighttpd)
+	 * @param array $cmd_params command parameters
+	 * @return array command result details
+	 */
+	public function enableHTTPS(string $repository_type, string $web_server, array $cmd_params): array
+	{
+		$cmd = [];
+		if ($web_server == Miscellaneous::WEB_SERVERS['nginx']['id']) {
+			$cmd = self::getEnableHTTPSNginxCommand(
+				$repository_type,
+				$cmd_params
+			);
+		} elseif ($web_server == Miscellaneous::WEB_SERVERS['lighttpd']['id']) {
+			$cmd = self::getEnableHTTPSLighttpdCommand(
+				$cmd_params
+			);
+		} elseif ($web_server == Miscellaneous::WEB_SERVERS['apache']['id']) {
+			$cmd = self::getEnableHTTPSApacheCommand(
+				$repository_type,
+				$cmd_params
+			);
+		}
+		$result = $this->execCommand($cmd, $cmd_params);
+		return $result;
+	}
+
+	/**
+	 * Disable HTTPS protocol in web server configuration file.
+	 *
+	 * @param string $repository_type system repository type (rpm, deb)
+	 * @param string $web_server web server name (apache, nginx, lighttpd)
+	 * @param array $cmd_params command parameters
+	 * @return array command result details
+	 */
+	public function disableHTTPS(string $repository_type, string $web_server, array $cmd_params): array
+	{
+		$cmd = [];
+		if ($web_server == Miscellaneous::WEB_SERVERS['nginx']['id']) {
+			$cmd = self::getDisableHTTPSNginxCommand(
+				$repository_type,
+				$cmd_params
+			);
+		} elseif ($web_server == Miscellaneous::WEB_SERVERS['lighttpd']['id']) {
+			$cmd = self::getDisableHTTPSLighttpdCommand(
+				$cmd_params
+			);
+		} elseif ($web_server == Miscellaneous::WEB_SERVERS['apache']['id']) {
+			$cmd = self::getDisableHTTPSApacheCommand(
+				$repository_type,
+				$cmd_params
+			);
+		}
+		$result = $this->execCommand($cmd, $cmd_params);
+		return $result;
+	}
+
+	/**
+	 * Reload web server configuration.
+	 * In case Lighttpd there is called restart action, not reload.
+	 *
+	 * @param string $repository_type system repository type (rpm, deb)
+	 * @param string $web_server web server name (apache, nginx, lighttpd)
+	 * @param array $cmd_params command parameters
+	 * @return array command result details
+	 */
+	public function reloadConfig(string $repository_type, string $web_server, array $cmd_params): array
+	{
+		$cmd = [];
+		if ($web_server == Miscellaneous::WEB_SERVERS['nginx']['id']) {
+			$cmd = self::getNginxReloadCommand(
+				$cmd_params
+			);
+		} elseif ($web_server == Miscellaneous::WEB_SERVERS['lighttpd']['id']) {
+			$cmd = self::getLighttpdRestartCommand(
+				$cmd_params
+			);
+		} elseif ($web_server == Miscellaneous::WEB_SERVERS['apache']['id']) {
+			$cmd = self::getApacheReloadCommand(
+				$repository_type,
+				$cmd_params
+			);
+		}
+		$result = $this->execCommand($cmd, $cmd_params);
+		return $result;
+	}
+
+
+	/**
+	 * Set port in web server configuration.
+	 *
+	 * @param string $repository_type system repository type (rpm, deb)
+	 * @param string $web_server web server name (apache, nginx, lighttpd)
+	 * @param integer $port port to set
+	 * @param array $cmd_params command parameters
+	 * @return array command result details
+	 */
+	public function setPort(string $repository_type, string $web_server, int $port, array $cmd_params): array
+	{
+		$cmd = [];
+		if ($web_server == Miscellaneous::WEB_SERVERS['nginx']['id']) {
+			$cmd = self::getChangeNginxPortCommand(
+				$repository_type,
+				$port,
+				$cmd_params
+			);
+		} elseif ($web_server == Miscellaneous::WEB_SERVERS['lighttpd']['id']) {
+			$cmd = self::getChangeLighttpdPortCommand(
+				$port,
+				$cmd_params
+			);
+		} elseif ($web_server == Miscellaneous::WEB_SERVERS['apache']['id']) {
+			$cmd = self::getChangeApachePortCommand(
+				$repository_type,
+				$port,
+				$cmd_params
+			);
+		}
+		$result = $this->execCommand($cmd, $cmd_params);
+		return $result;
 	}
 }
